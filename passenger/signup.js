2485e78e874f2b0a712e7a5f3fed38cbbe0b5c4d
Jan Paul Batrina <jmbatrina@up.edu.ph>
2024-01-10 01:25:18 +0800

    Passenger: add signup process

---
+++ passenger/signup.js
@@ -0,0 +1,123 @@
+function loadAccountDB() {
+    let account_db = JSON.parse(localStorage.getItem("subeo-accounts")) ??
+        {"('juandelacruz','password')": {
+            "email": "juandelacruz@example.com"
+        }};
+
+    return account_db;
+}
+
+function getUsernamesAndEmails(account_db) {
+    var usernames = [];
+    var emails = [];
+    for (let [key, details] of Object.entries(account_db)) {
+        let username = key.substring(2, key.indexOf("'", 2));
+        let email = details["email"];
+
+        if (username) usernames.push(username);
+        if (email) emails.push(email);
+    }
+
+    return [usernames, emails];
+}
+
+function registerCredentials() {
+    var username = document.getElementById("username")?.value ?? "";
+    var password = document.getElementById("password")?.value ?? "";
+    var confirm = document.getElementById("confirm-password")?.value ?? "";
+    var email = document.getElementById("email")?.value ?? "";
+
+    // NOTE: for simplicity, we just use the username and password as key
+    // TODO: Use proper auth
+    var key = `('${username}','${password}')`;
+    console.log("account" + key)
+    let [usernames, emails] = getUsernamesAndEmails(loadAccountDB());
+    if (usernames.indexOf(username) != -1) {
+        alert(`Account with username ${username} already exists. Please choose another username`);
+    } else if (emails.indexOf(email) != -1) {
+        alert(`An account has already been linked with the email ${email}. If you forgot your password, please contact Subeo support to retrieve your account`);
+        return;
+    } else if (password != confirm) {
+        alert("Password and Confirm Password do not match.");
+        return;
+    }
+
+    let accountInfo = {
+        "username": username,
+        "password": password,
+        "email": email
+    }
+
+    sessionStorage.setItem("subeo-newAccount-details", JSON.stringify(accountInfo));
+    alert(sessionStorage.getItem("subeo-newAccount-details"));
+    window.location.href = "signup-2.html";
+}
+
+function registerDetails(accountInfo) {
+    let key =`('${accountInfo["username"]}','${accountInfo["password"]}')`;
+
+    let detailIDs = ["first-name", "mid-name", "last-name", "age"];
+    let imgIDs = ["1by1ID", "proofID", "kycID"];
+    let accountDetails = {}
+    accountDetails["email"] = accountInfo["email"];
+
+    for (let detailID of detailIDs) {
+        accountDetails[detailID] = document.getElementById(detailID)?.value ?? "";
+    }
+
+    for (let imgID of imgIDs) {
+        accountDetails[imgID] = document.getElementById(imgID)?.getAttribute("imgBase64") ?? "";
+    }
+
+    // TODO: optimize by not requiring full reload/resave of DB
+    console.log(accountDetails);
+    let account_db = loadAccountDB();
+    account_db[key] = accountDetails;
+    localStorage.setItem("subeo-accounts", JSON.stringify(account_db));
+
+    // TODO: make sure transaction numbers are unique
+    let transactionNumber = getRandomIntInclusive(0, "99999999");
+    alert(`Your details have been submitted. Your transaction number is ${transactionNumber}. Kindly await confirmation email for your account)`);
+    window.location.href = "index.html";
+}
+
+function passengerTypeChanged() {
+    let passengerType = document.getElementById("passenger-type")?.value ?? 0;
+    var passengerString = [
+        "Student", "Senior Citizen", "PWD"
+    ][passengerType];
+    document.getElementById("proof-id-label").innerText = `${passengerString} ID: `;
+    document.getElementById("kyc-id-label").innerText = `Selfie with ${passengerString} ID: `;
+
+    document.getElementById("div-proof-id").removeAttribute("hidden");
+}
+
+function addImagePreview(inputElemID) {
+    let inputElem = document.getElementById(inputElemID);
+    const imgElemID = `${inputElemID}-img`;
+    inputElem?.insertAdjacentHTML("afterend",
+    `<br>
+    <img src="" id="${imgElemID}" height="300" hidden/>
+    `);
+
+    inputElem.addEventListener('change', (event) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(event.target.files[0]);
+        reader.addEventListener("load", () => {
+            let imgElem = document.getElementById(imgElemID);
+            imgElem.src = reader.result;
+            imgElem.removeAttribute("hidden");
+
+            const imgAsBase64 = reader.result.replace('data:', '').replace(/^.+,/, '');
+            inputElem.setAttribute("imgBase64", imgAsBase64);
+        });
+    });
+}
+
+
+// FROM: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values_inclusive
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
